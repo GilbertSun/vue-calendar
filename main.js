759cf15a33edce5fcd436eccdbce534a6917753b
GilbertSun <szb4321@gmail.com>
2016-11-08 18:14:58 +0800

    Updates

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 import Vue from 'vue'
-import Calendar from '../src/calendar.vue'
+import App from './app.vue'
 
 new Vue({
   el: 'body',
-  data: {
-    selected: new Date(),
-    decorate: {
-      '2016-08-09': true,
-      '2016-08-10': true,
-      '2016-08-11': true
-    },
-    headerBackgroundColor: '#77c3f4',
-    currentView: {}
-  },
   components: {
-    Calendar
+    App
   }
 })
